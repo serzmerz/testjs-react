9aa353cf1fd761a79341d26f6fe14cf62e46eb98
Sergey Tanskiy <serzmerz@gmail.com>
2017-07-20 17:57:40 +0300

    react-materialize added

--- src/conteiners/CreateOrder/index.js
+++ src/conteiners/CreateOrder/index.js
@@ -1,40 +1,60 @@
 import React, { Component } from 'react';
+import {Row, Input} from 'react-materialize';
 
 class CreateOrder extends Component {
     render() {
         return (
-            <div className="row">
-                <form className="col s12">
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="first_name" type="text" className="validate"/>
-                                <label htmlFor="first_name">First Name</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="last_name" type="text" className="validate"/>
-                                <label htmlFor="last_name">Last Name</label>
-                        </div>
+        <div>
+        <div className="row">
+            <div className="col s12 m12">
+                <div className="card">
+                    <div className="card-content">
+                        <span className="card-title">Заказчик</span>
+                        <div className="divider"> </div>
+                        <form>
+                            <div className="row">
+                                <div className="input-field col s6">
+                                    <input id="first_name" type="text" className="validate"/>
+                                    <label htmlFor="first_name">First Name</label>
+                                </div>
+                                <div className="input-field col s6">
+                                    <input id="last_name" type="text" className="validate"/>
+                                    <label htmlFor="last_name">Last Name</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input disabled id="disabled" type="text" className="validate"/>
+                                    <label htmlFor="disabled">Disabled</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s6">
+                                    <input id="email" type="email" className="validate"/>
+                                    <label htmlFor="email">Email</label>
+                                </div>
+                                <div className="input-field col s6">
+                                    <input id="phone" type="text" className="validate"/>
+                                    <label htmlFor="phone">Phone number</label>
+                                </div>
+                            </div>
+
+                            <h5>Заказ</h5>
+                            <div className="divider"> </div>
+                            <Row>
+                                <Input placeholder="Placeholder" s={6} label="First Name" />
+                            </Row>
+
+                        </form>
                     </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input disabled id="disabled" type="text" className="validate"/>
-                                <label htmlFor="disabled">Disabled</label>
-                        </div>
+                    <div className="card-action">
+                        <a href="#">This is a link</a>
                     </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate"/>
-                                <label htmlFor="password">Password</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="email" type="email" className="validate"/>
-                                <label htmlFor="email">Email</label>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
+        </div>
+
+        </div>
         );
     }
 }
